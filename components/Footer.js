c44e03f3d59929557144181a2546709dc56e7618
jpfraneto <jpfraneto@gmail.com>
2023-02-20 12:52:30 -0300

    Yiajuuu0

--- components/Footer.js
+++ components/Footer.js
@@ -17,11 +17,8 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       Nothing in here is directly associated with Yuga Labs. I'm just a random
-      person that owns deed 60768 and is going to build something awesome in it.
-      More about me on{'  '}
-      <span className={styles.twitterLink} onClick={handleTwitterLink}>
-        Twitter
-      </span>{' '}
+      person who owns deed 60768 and is going to build something awesome in it.
+      More about me on Twitter at @kithkui.
     </div>
   );
 };
